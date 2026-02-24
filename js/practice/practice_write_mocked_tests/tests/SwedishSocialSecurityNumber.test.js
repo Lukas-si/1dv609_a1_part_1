8601fc7acbbaa485339fe5bfc069a75cf9a206fd
Lukas <lukas.soderlund.fanta@gmail.com>
2026-02-24 20:59:51 +0100

    don't know how to do this :(

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,14 +1,83 @@
-import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'; 
+import { describe, expect, jest, test } from '@jest/globals'
 
 
 
-//NOTE THESE TESTS SHOULD NOT BE DEPENDENT ON SSNHelper BUT USE MOCKING
-describe('SwedishSocialSecurityNumber Tests', () => {
-    //put constants here to increase readability
+import { SwedishSocialSecurityNumber } from "../src/correct/SwedishSocialSecurityNumber"
+//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'
+//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'
+//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberNoLuhn'
+//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongYear'
 
-    test('replace this test with one of your own', () => {
-        expect(true).toBe(true);
-    });
+describe("SwedishSocialSecurityNumber Tests", () => {
 
-    //Add your tests here
-});
+  const correctFormat = "890201-3286"
+  const correctFormatWithSpaces = " 890201-3286 "
+  const wrongFormat = " 890202341-32286 "
+
+  let mockHelper
+
+  beforeEach(() => {
+
+   mockHelper = {
+    isCorrectLength: jest.fn(),
+    isCorrectFormat: jest.fn(),
+    isValidMonth: jest.fn(),
+    isValidDay: jest.fn(),
+    luhnisCorrect: jest.fn(),
+  }
+
+})
+
+  test('should throw if length is incorrect', () => {
+    mockHelper.isCorrectLength.mockReturnValue(false)
+
+    expect(() => {
+      new SwedishSocialSecurityNumber(correctFormat, mockHelper)
+    }).toThrow("To short, must be 11 characters")
+  })
+
+  test('should trim the whitespaces', () => {
+    const result = new SwedishSocialSecurityNumber(correctFormatWithSpaces, mockHelper)
+
+    expect(result.getSerialNumber()).toBe('3286')
+  })
+
+  test('should not throw error when Luhn validation fails', () => {
+    mockHelper.luhnisCorrect.mockReturnValue(false)
+
+    expect(() => {
+        new SwedishSocialSecurityNumber(correctFormat, mockHelper)
+    }).toThrow("Invalid SSN according to Luhn's algorithm")
+  })
+
+  test('should get the correct year when the SSN is valid', () => {
+    const result = new SwedishSocialSecurityNumber(correctFormat, mockHelper)
+
+    expect(result.getYear()).toBe('89')
+  })
+
+  test('should throw when incorrect format is inputed', () => {
+    mockHelper.isCorrectFormat.mockReturnValue(false)
+
+    expect(() => {
+        new SwedishSocialSecurityNumber(wrongFormat, mockHelper)
+    }).toThrow("Incorrect format, must be: YYMMDD-XXXX")
+  })
+
+  test('should throw when incorrect day is inputed', () => {
+    mockHelper.isValidDay.mockReturnValue(false)
+
+    expect(() => {
+        new SwedishSocialSecurityNumber(wrongFormat, mockHelper)
+    }).toThrow("Invalid month in SSN")
+  })
+
+  test('should throw when incorrect month is inputed', () => {
+    mockHelper.isValidMonth.mockReturnValue(false)
+
+    expect(() => {
+        new SwedishSocialSecurityNumber(wrongFormat, mockHelper)
+    }).toThrow("Invalid month in SSN")
+  })
+
+})
