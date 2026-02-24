19ee0f9e8e9a04818fc5edc8701355c81de19a47
Lukas <lukas.soderlund.fanta@gmail.com>
2026-02-24 15:49:56 +0100

    Passed the SSN buggs

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -1,15 +1,38 @@
 import { SSNHelper } from '../src/correct/SSNHelper'; 
-//import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowDayUpTo30';
-
+// import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowDayUpTo30';
+// import { SSNHelper } from "../src/bugs/BuggySSNHelperAllowMonth0";
+// import { SSNHelper } from "../src/bugs/BuggySSNHelperIncorrectFormat";
+// import { SSNHelper } from "../src/bugs/BuggySSNHelperWrongLength";
 
 
 describe('SSNHelpe Tests', () => {
-    const exactlyDays = 31
-    test('Should be inValid beacuse not <= ', () => {
-        const helper = () => new SSNHelper(exactlyDays)
-        expect(helper).toBe(true)
-    })
 
- 
-    //Add your tests here
+test('Should be valid if <= 31', () => {
+    const helper = new SSNHelper()
+    const result = helper.isValidDay('31')
+
+    expect(result).toBe(true)
+})
+
+test('Should be valid if == 11', () => {
+    const helper = new SSNHelper()
+    const result = helper.isCorrectLength('12345678765434567')
+
+    expect(result).toBe(false)
+})
+
+test('Should be valid if correct format with - ', () => {
+    const helper = new SSNHelper()
+    const result = helper.isCorrectFormat('0111080594')
+
+    expect(result).toBe(false)
+})
+
+test('Should fail beacuse of length', () => {
+    const helper = new SSNHelper()
+    const result = helper.isCorrectLength('1234567891011')
+
+    expect(result).toBe(false)
+})
+
 });
